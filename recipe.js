87b743715ebff1e6cbb95ba9afd424e69399f862
rafeeq-123 <rafeeq-123@hotmail.com>
2016-08-22 20:23:11 -0700

    Done.

--- recipe.js
+++ recipe.js
@@ -1,1 +1,22 @@
 'use strict';
+function addIngredient(recipeObject,ingredientName,amounts ) {
+  recipeObject[ingredientName] = amounts;
+  return recipeObject;
+
+}
+
+function removeIngredient(recipeObject, ingredientName){
+  delete recipeObject[ingredientName];
+  return recipeObject;
+}
+
+function updateIngredient(recipeObject,ingredientName,amounts){
+  recipeObject[ingredientName] = '3 cups';
+  return recipeObject;
+}
+
+function readRecipe(recipeObject){
+  for (var key in recipeObject) {
+    console.log("this recipe calls for " + recipeObject[key] + " of " + key );
+  }
+}
